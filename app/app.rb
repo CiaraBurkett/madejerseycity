080c0f04a1f53cb2ef5a677747008acd4c77e53a
Ciara Burkett <ciaraburkett@gmail.com>
2013-03-14 13:59:02 -0400

    Sequel create table

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,16 @@ require "sequel"
 require "sqlite3"
 require "slim"
 
+DB = Sequel.sqlite
+
+DB.create_table :listings do
+	primary_key :id 
+	String :name
+	String :url
+	String :description
+	TrueClass :hiring
+end
+
 get '/' do
 	slim :index
 end
