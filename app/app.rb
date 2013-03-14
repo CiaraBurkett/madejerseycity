1c54869c74038362b468f21826efccf5fdce7b7c
Ciara Burkett <ciaraburkett@gmail.com>
2013-03-14 14:08:16 -0400

    Add comments

--- app/app.rb
+++ app/app.rb
@@ -7,12 +7,18 @@ get '/' do
 	slim :index
 end
 
+# connect to an in-memory database
 DB = Sequel.sqlite
 
+# create a listings table
 DB.create_table :listings do
 	primary_key :id 
 	String :name
 	String :url
 	String :description
 	TrueClass :hiring
+end
+
+class App 
+	
 end
