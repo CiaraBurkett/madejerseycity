d00ead5588fcf76a19b88695aeb7cfd89c35ab8c
Ciara Burkett <ciaraburkett@gmail.com>
2013-03-18 12:29:34 -0400

    Change database name to entries

--- app/app.rb
+++ app/app.rb
@@ -3,20 +3,20 @@ require "sequel"
 require "sqlite3"
 require "slim"
 
-# # connect to an in-memory database
-# DB = Sequel.sqlite
+# connect to an in-memory database
+DB = Sequel.sqlite
 
-# # create a listings table
-# DB.create_table :listings do
-# 	primary_key :id 
-# 	String :name
-# 	String :url
-# 	String :description
-# 	TrueClass :hiring
-# end
+# create a entries table
+DB.create_table :entries do
+	primary_key :id 
+	String :name
+	String :url
+	String :description
+	TrueClass :hiring
+end
 
-# # create a dataset from the listings table
-# listings = DB[:listings]
+# create a dataset from the entries table
+entries = DB[:entries]
 
 get '/' do
 	slim :index
